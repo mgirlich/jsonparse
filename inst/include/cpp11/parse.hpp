caadac610b298e47daf9614b3f1dcebc975884b8
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-09-29 09:53:55 +0000

    Move files from src to inst folder

--- inst/include/cpp11/parse.hpp
+++ inst/include/cpp11/parse.hpp
@@ -27,7 +27,7 @@ int parse_scalar_int(simdjson::ondemand::value element, const std::string& path)
     switch (element.type()) {
     case json_type::number:
         // TODO this is unsafe; add handling of big integers
-        return int(int64_t(element));
+        return static_cast<int>(int64_t(element));
         break;
     case json_type::null:
         return NA_INTEGER;
@@ -42,10 +42,10 @@ double parse_scalar_double(simdjson::ondemand::value element, const std::string&
 
     switch (element.type()) {
     case json_type::number:
-        return double(element);
+        return static_cast<double>(element);
         break;
     case json_type::null:
-        return NA_REAL;
+        return static_cast<double>(NA_REAL);
         break;
     default:
         throw std::runtime_error(bad_json_type_message(element, "double", path));
@@ -65,4 +65,67 @@ SEXPREC* parse_scalar_string(simdjson::ondemand::value element, const std::strin
     default:
         throw std::runtime_error(bad_json_type_message(element, "string", path));
     }
+}
+SEXP parse_homo_array_bool(simdjson::ondemand::value json, const std::string& path) {
+    simdjson::ondemand::array array = safe_get_array(json, path);
+
+    int n = array.count_elements();
+    SEXP val = PROTECT(Rf_allocVector(LGLSXP, n));
+
+    int i = 0;
+    for (auto element : array) {
+        SET_LOGICAL_ELT(val, i, parse_scalar_bool(element.value(), path));
+        i++;
+    }
+
+    UNPROTECT(1);
+    return val;
+}
+
+SEXP parse_homo_array_int(simdjson::ondemand::value json, const std::string& path) {
+    simdjson::ondemand::array array = safe_get_array(json, path);
+
+    int n = array.count_elements();
+    SEXP val = PROTECT(Rf_allocVector(INTSXP, n));
+
+    int i = 0;
+    for (auto element : array) {
+        SET_INTEGER_ELT(val, i, parse_scalar_int(element.value(), path));
+        i++;
+    }
+
+    UNPROTECT(1);
+    return val;
+}
+
+SEXP parse_homo_array_double(simdjson::ondemand::value json, const std::string& path) {
+    simdjson::ondemand::array array = safe_get_array(json, path);
+
+    int n = array.count_elements();
+    SEXP val = PROTECT(Rf_allocVector(REALSXP, n));
+
+    int i = 0;
+    for (auto element : array) {
+        SET_REAL_ELT(val, i, parse_scalar_double(element.value(), path));
+        i++;
+    }
+
+    UNPROTECT(1);
+    return val;
+}
+
+SEXP parse_homo_array_string(simdjson::ondemand::value json, const std::string& path) {
+    simdjson::ondemand::array array = safe_get_array(json, path);
+
+    int n = array.count_elements();
+    SEXP val = PROTECT(Rf_allocVector(STRSXP, n));
+
+    int i = 0;
+    for (auto element : array) {
+        SET_STRING_ELT(val, i, parse_scalar_string(element.value(), path));
+        i++;
+    }
+
+    UNPROTECT(1);
+    return val;
 }
